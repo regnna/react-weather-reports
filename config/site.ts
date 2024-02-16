b33db931bcc8812cf6d207dccf5fc8c96f8fa98a
regnna <ritmchats@gmail.com>
2024-02-17 02:07:42 +0530

    respo:responsive searchBox

--- config/site.ts
+++ config/site.ts
@@ -4,12 +4,12 @@ export const siteConfig = {
   name: "Weathers",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
+  // mainNav: [
+  //   {
+  //     // title: "Home",
+  //     // href: "/",
+  //   },
+  // ],
   links: {
     twitter: "https://twitter.com/knobodykares",
     github: "https://github.com/regnna",
