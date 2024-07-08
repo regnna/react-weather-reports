157ec8850df2ecbbda06e1cbff7f2a536c12ae0c
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2024-07-08 21:15:58 +0530

    chore: navbar alignment

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -124,7 +124,7 @@ function handleCurrentLocation(){
         <LocateFixed
               title="Your Current Location"
               onClick={handleCurrentLocation}
-              className=" cursor-pointer"
+              className=" cursor-pointer ml-5 px-2"
             />
         {/* <Link
         title="Your current Location"
@@ -148,11 +148,7 @@ function handleCurrentLocation(){
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-locate-fixed"><line x1="2" x2="5" y1="12" y2="12"/><line x1="19" x2="22" y1="12" y2="12"/><line x1="12" x2="12" y1="2" y2="5"/><line x1="12" x2="12" y1="19" y2="22"/><circle cx="12" cy="12" r="7"/><circle cx="12" cy="12" r="3"/></svg>
         </div> */}
         <div className="flex items-center justify-center h-5 w-5 mt-3">
-        <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
+       
             <div
                 className={buttonVariants({
                   size: "icon",
@@ -163,9 +159,9 @@ function handleCurrentLocation(){
                 <Icons.location className="h-5 w-5 fill-current" />
                 <span className="sr-only">Location</span>
               </div>
-            </Link>
+       
         
-  <p className=" flex text-foreground capitalize ">{place}</p>
+  <p className=" flex text-foreground capitalize px-4">{place}</p>
 </div>
           {/* <div></div> */}
           {/* <nav className="flex items-center space-x-1"> */}
